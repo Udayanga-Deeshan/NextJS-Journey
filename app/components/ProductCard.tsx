e6ee216669610db46b7c8fda3085ce22a393b5de
Udayanga-Deeshan <Udayangadilshan1789@gmail.com>
2025-05-06 20:20:39 +0530

    server side components & client side components

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,16 @@
+
+import { log } from 'console'
+import React from 'react'
+import AddToCart from './AddToCart'
+
+
+const ProductCard = () => {
+  return (
+    <div>
+       <h2>Product card</h2>
+       <AddToCart/>
+    </div>
+  )
+}
+
+export default ProductCard
